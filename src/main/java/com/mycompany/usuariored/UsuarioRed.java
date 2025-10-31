0ee63ead163c739454e58c267229a9eb1f4b7b80
AndyCamargo <andycamar001@gmail.com>
2025-10-30 23:38:10 -0500

    aplicacion completa

---
+++ src/main/java/com/mycompany/usuariored/UsuarioRed.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usuariored;
+
+/**
+ *
+ * @author user
+ */
+public class UsuarioRed {
+
+    public static void main(String[] args) {
+    
+        
+          // Ejecuta la ventana de Inicio
+        java.awt.EventQueue.invokeLater(() -> {
+            new Inicio().setVisible(true);
+        });
+        
+    
+    }
+}
